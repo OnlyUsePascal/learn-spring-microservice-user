371503899418f0f490d16afc483ff944e68648bf
joun <chuyengianhorang@gmail.com>
2025-10-19 17:05:54 +0700

    sign up

--- src/main/java/com/example/user/UserController.java
+++ src/main/java/com/example/user/UserController.java
@@ -1,12 +1,15 @@
 package com.example.user;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -25,7 +28,17 @@ public class UserController {
     var u = userRepo.findByUsername(username);
     if (u.isEmpty())
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with username:" + username);
-    
+
     return ResponseEntity.ok(u.get());
+  }
+
+  @PostMapping("/")
+  public ResponseEntity<UUID> createUser(@RequestBody CreateUserDto dto) {
+    var user = userRepo.save(User.builder()
+        .email(dto.getEmail())
+        .username(dto.getUsername())
+        .password(dto.getPassword())
+        .build());
+    return ResponseEntity.ok(user.getId());
   }
 }
